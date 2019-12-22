ac4108d2e3c6f90c312955b8b84ad155c7380a9a
Rubel2475 <37175213+Rubel2475@users.noreply.github.com>
2019-12-21 23:30:52 -0800

    Add files via upload

---
+++ banker's_Algorithm.cpp
@@ -0,0 +1,98 @@
+// Banker's Algorithm
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+	int n,m,i,j,k;
+
+	cout << "Enter the number of processes: ";
+	cin >> n;
+	cout << "Enter the number of resourses: ";
+	cin >> m;
+
+	int TotalIns[m]={0},available[m]={0}, res[m]={0};
+
+	cout<< "Enter the total instances: \n";
+	for(i=0; i<m; i++){
+        cin>> TotalIns[i];
+    }
+
+	int allocate[5][3] = { { 0, 1, 0 }, // P0 // Allocation Matrix
+						{ 2, 0, 0 }, // P1
+						{ 3, 0, 2 }, // P2
+						{ 2, 1, 1 }, // P3
+						{ 0, 0, 2 } }; // P4
+
+	int max_demand[5][3] = { { 7, 5, 3 }, // P0 // MAX Matrix
+					{ 3, 2, 2 }, // P1
+					{ 9, 0, 2 }, // P2
+					{ 2, 2, 2 }, // P3
+					{ 4, 3, 3 } }; // P4
+
+
+
+	/* int allocate[n][m];
+	int max_demand[n][m];
+
+	for(i=0; i<n; i++){
+        for(j=0; j<m; j++){
+            cin>> allocate[i][j];
+        }
+	}
+
+	for(i=0; i<n; i++){
+        for(j=0; j<m; j++){
+            cin>> max_demand[i][j];
+        }
+	}*/
+
+	for(i=0; i<m; i++){
+        for(j=0; j<n; j++){
+            res[i] =  res[i] + allocate[j][i];
+        }
+        available[i] = TotalIns[i] - res[i];
+	}
+
+	int finish[n], ans[n], ind=0;
+
+	for(k=0; k<n; k++){
+		finish[k]=0;
+	}
+
+	int need[n][m];
+	for(i=0; i<n; i++) {
+		for(j=0; j<m; j++)
+			need[i][j] = max_demand[i][j] - allocate[i][j];
+	}
+
+	int y=0;
+	for(k=0; k<5; k++){
+		for(i=0; i<n; i++){
+			if(finish[i]==0){
+				int flag=0;
+				for(j=0; j<m; j++){
+					if(need[i][j] > available[j]){
+						flag=1;
+						break;
+					}
+				}
+
+				if (flag==0){
+					ans[ind++]=i;
+					for(y=0; y<m; y++)
+						available[y] += allocate[i][y];
+                        finish[i] = 1;
+				}
+			}
+		}
+	}
+
+	printf("The safe Sequence: ");
+	for (i=0; i<n-1; i++)
+		printf("  P%d ->", ans[i]);
+	printf(" P%d\n", ans[n-1]);
+
+	return (0);
+}
+
