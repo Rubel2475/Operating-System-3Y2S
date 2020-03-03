6aca3acfab2d468b7ec0704c3959a140797f6f5d
Rubel2475 <37175213+Rubel2475@users.noreply.github.com>
2020-03-03 20:57:57 +0600

    Easier program added ( the later one)

--- Final Labwork/CPUScheduling_RoundRobin.cpp
+++ Final Labwork/CPUScheduling_RoundRobin.cpp
@@ -66,3 +66,91 @@ int main()
 
 	return 0;
 }
+
+
+//code without typedefine
+
+//Round robin CPU Scheduling for some processes of same arrival time=0
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n,q, pro[20], waitT[20], burstT[20], main_burstT[20], completionT[20], turnAT[20];
+    float total_WT=0,total_TAT=0;
+
+    cout << "Enter number of processes: ";
+    cin >> n;
+
+    cout<<"Enter the value of time quantum: ";
+	cin>>q;
+
+    for(int i=0; i<n; i++){
+        cout << "Enter process burst time: ";
+        cout << "p" << i+1 << ": ";
+        cin >> burstT[i];
+        pro[i] = i+1;
+        main_burstT[i] = burstT[i];
+    }
+
+    int pcom=0; //process completed
+    int ctm=0; //current time
+
+    while(pcom<n){
+
+        for(int j=0; j<n; j++){
+            if(burstT[j]>q){
+                burstT[j] = burstT[j]-q;
+                ctm = ctm + q;
+            }
+            else if(burstT[j]>0){
+                ctm = ctm + burstT[j];
+                completionT[j] = ctm;
+                pcom++;
+                burstT[j]=0;
+            }
+        }
+
+    }
+
+    for(int i=0; i<n; i++){
+
+        waitT[i] = completionT[i] - main_burstT[i];
+        total_WT = total_WT + waitT[i];
+
+        turnAT[i] = waitT[i] + main_burstT[i];
+        total_TAT = total_TAT + turnAT[i];
+    }
+
+    cout<< "\nProcess\t Burst time\t completion time \tWaiting time\tTurnaroundtime\n";
+    for(int i=0; i<n; i++){
+        cout << pro[i] << "\t" << main_burstT[i] << "\t\t" << completionT[i] << "\t\t\t" << waitT[i] << "\t\t\t" << turnAT[i] << endl;
+    }
+
+    cout << "\nAverage waiting time: " << total_WT/n << endl;
+    cout << "\naverage  turnaroundtime: " << total_TAT/n << endl;
+
+    return 0;
+}
+
+
+
+/*
+6 --process
+3 -- time quantam
+5 ...burst time
+6
+7
+9
+2
+3
+*/
+
+
+
+
+
+
+
+
